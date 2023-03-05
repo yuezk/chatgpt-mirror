7f1aa0beccfc5c359cef81b8dea89ccf9d542d61
Kevin Yue <yuezk001@gmail.com>
2023-03-05 18:32:49 +0800

    Enable compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import compression from 'compression';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
